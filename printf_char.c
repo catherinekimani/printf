cae9cead9dee7b4628c1528047a22a91117f50e1
catherinekimani <catherinekimani882@gmail.com>
2023-03-27 02:31:27 +0300

    feat: Add printf_char function to print a single character to stdout

---
+++ printf_char.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_char - prints a single character to stdout
+ * @arg: the argument list (va_list) containing the character to print
+ *
+ * Return: the num of characters printed
+ */
+int printf_char(va_list arg)
+{
+	char c = va_arg(arg, int);
+	putchar(c);
+	return (1);
+}
