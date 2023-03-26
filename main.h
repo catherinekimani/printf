1ec654d357709482daab1458e3314d8ae1220f2b
catherinekimani <catherinekimani882@gmail.com>
2023-03-27 01:58:38 +0300

    Feat: Create Header file defining a struct and function prototypes for printf-like functionality

---
+++ main.h
@@ -0,0 +1,28 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <limits.h>
+#include <unistd.h>
+
+/**
+ * struct ConvSpec - represents a conversion specifier in a printf format string
+ * @spec: the conversion specifier character, such as 'd', 'f', 's', etc.
+ * @func: pointer to the function that implements the conversion for this specifier
+ */
+typedef struct ConvSpec
+{
+	char *spec;
+	int (*func)();
+} ConvSpec;
+
+/*Prototypes*/
+int _printf(const char *format, ...);
+int printf_char(va_list arg);
+int printf_string(va_list arg);
+int printf_percent(va_list arg);
+int printf_integer(va_list arg);
+int _putchar(char c);
+#endif /*MAIN_H*/
