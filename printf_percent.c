b4d8b0f7dd2768eca1ba95413704449cc7f91bea
catherinekimani <catherinekimani882@gmail.com>
2023-03-27 22:30:15 +0300

    Ft: Update string, percent char, and integer files to support flags, width, precision, size and buffer

--- printf_percent.c
+++ printf_percent.c
@@ -1,12 +1,23 @@
 #include "main.h"
 
 /**
- * printf_percent - prints the char 37
- *
- * Return: the number of characters printed
+ * printf_percent - Prints a percent sign
+ * @types: Lista of arguments
+ * @buffer: Buffer array to handle print
+ * @flags:  Calculates active flags
+ * @width: get width.
+ * @precision: Precision specification
+ * @size: Size specifier
+ * Return: Number of chars printed
  */
-int printf_percent(void)
+int printf_percent(va_list types, char buffer[],
+	int flags, int width, int precision, int size)
 {
-	_putchar(37);
-	return (1);
+	UNUSED(types);
+	UNUSED(buffer);
+	UNUSED(flags);
+	UNUSED(width);
+	UNUSED(precision);
+	UNUSED(size);
+	return (write(1, "%%", 1));
 }
